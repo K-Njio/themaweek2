e7a037f120f3e559491a10afe686b3eb96f3e115
K-Njio <302767830@student.rocmondriaan.nl>
2024-01-19 22:21:26 +0100

    .

--- test2/main.js
+++ test2/main.js
@@ -61,11 +61,12 @@ function generateObstacle() {
 
         if (obstacleLeft === -60) {
             obstacle.classList.remove("obstacle")
+            gameDisplay.removeChild("obstacle")
         }
 
         if (
             obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
-             (birdBottom < obstacleBottom + 150 && birdBottom > obstacleBottom + gap -200)||
+             (birdBottom < obstacleBottom + 150 || birdBottom > obstacleBottom + gap -200)||
             birdBottom === 0 
             ) {
             gameOver()
@@ -74,7 +75,7 @@ function generateObstacle() {
     }
 
     let timerObstacle = setInterval(moveObstacle, 20)
-    if(!gameIsOver){setTimeout(generateObstacle, 3000)}
+    if(!gameIsOver){setTimeout(generateObstacle, 3500)}
 }
 
 generateObstacle() 
