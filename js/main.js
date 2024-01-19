17fd8c74846d7cfd2c3287a8896fbec9af734631
K-Njio <302767830@student.rocmondriaan.nl>
2024-01-19 23:40:21 +0100

    ;

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 console.log("kaas");
 
+const bird = document.querySelector(".bird");
+const gameDisplay = document.querySelector(".game-container");
+const ground = document.querySelector(".ground");
 
-const bird = document.querySelector(".bird")
-const gameDisplay = document.querySelector(".game-container")
-const ground = document.querySelector(".ground")
-
-let birdLeft = 220
-let birdBottom = 100
-let gravity = 2
-let gameIsOver= false
-let gap=460
+let birdLeft = 220;
+let birdBottom = 100;
+let gravity = 2;
+let gameIsOver = false;
+let gap = 460;
+let score = 0;
 
 function startGame() {
     birdBottom -= gravity
-    bird.style.bottom = birdBottom + 'px'
-    bird.style.left = birdLeft + 'px'
+    bird.style.bottom = birdBottom + 'px';
+    bird.style.left = birdLeft + 'px';
 }
-let timerGame=setInterval(startGame, 20)
+let timerGame = setInterval(startGame, 20)
 
 function jump() {
-    if (birdBottom < 500) birdBottom += 50
+    if (birdBottom < 500) birdBottom += 50;
     // bird.style.bottom=birdBottom + 'px'
 }
 //als je op de muis klik, dan kan je springen.
-document.addEventListener('click', jump)
+document.addEventListener('click', jump);
 
 //hiermee kan je met alle knoopen op toetsenbord springen
 // document.addEventListener('keydown', jump)
@@ -35,58 +35,55 @@ document.addEventListener('keydown', function (event) {
     }
 });
 
-
-
 function generateObstacle() {
-    let obstacleLeft = 500    
-    let randomHeight = Math.random() * 90
-    let obstacleBottom = randomHeight
+    let obstacleLeft = 500;
+    let randomHeight = Math.random() * 90;
+    let obstacleBottom = randomHeight;
 
-    const obstacle = document.createElement("div")
-    const topObstacle = document.createElement("div")
-  if(!gameIsOver){
-    obstacle.classList.add("obstacle")  
-    topObstacle.classList.add("top-obstacle")  
-}
-    gameDisplay.appendChild(obstacle)
-    gameDisplay.appendChild(topObstacle)
-    obstacle.style.left = obstacleLeft + "px"
-    topObstacle.style.left = obstacleLeft + "px"
-    obstacle.style.bottom = obstacleBottom + "px"
-    topObstacle.style.bottom = obstacleBottom + gap +"px"
+    const obstacle = document.createElement("div");
+    const topObstacle = document.createElement("div");
+    if (!gameIsOver) {
+        obstacle.classList.add("obstacle");
+        topObstacle.classList.add("top-obstacle");
+    }
+    gameDisplay.appendChild(obstacle);
+    gameDisplay.appendChild(topObstacle);
+    obstacle.style.left = obstacleLeft + "px";
+    topObstacle.style.left = obstacleLeft + "px";
+    obstacle.style.bottom = obstacleBottom + "px";
+    topObstacle.style.bottom = obstacleBottom + gap + "px";
 
-    function moveObstacle(){
-        obstacleLeft -= 2
-        obstacle.style.left = obstacleLeft + "px"
-        topObstacle.style.left = obstacleLeft + "px"
+    function moveObstacle() {
+        obstacleLeft -= 2;
+        obstacle.style.left = obstacleLeft + "px";
+        topObstacle.style.left = obstacleLeft + "px";
 
         if (obstacleLeft === 0) {
-            clearInterval(timerObstacle)
-            gameDisplay.removeChild(obstacle)
-            gameDisplay.removeChild(topObstacle)
+            clearInterval(timerObstacle);
+            gameDisplay.removeChild(obstacle);
+            gameDisplay.removeChild(topObstacle);
         }
-//waar de vogel de paal aanraakt gaat ie af.
+        //waar de vogel de paal aanraakt gaat ie af.
         if (
             obstacleLeft > 200 && obstacleLeft < 274 && birdLeft === 220 &&
-             (birdBottom < obstacleBottom + 178 || birdBottom > obstacleBottom + gap -205)||
-            birdBottom === 0 
-            ) {
+            (birdBottom < obstacleBottom + 178 || birdBottom > obstacleBottom + gap - 205) ||
+            birdBottom === 0) {
             gameOver()
             clearInterval(timerObstacle)
         }
     }
 
-    let timerObstacle = setInterval(moveObstacle, 20)
-    if(!gameIsOver){setTimeout(generateObstacle, 3500)}
+    let timerObstacle = setInterval(moveObstacle, 20);
+    if (!gameIsOver) { setTimeout(generateObstacle, 3500)}
 }
 
-generateObstacle() 
+generateObstacle()
 
 
 function gameOver() {
-    clearInterval(timerGame)
-    console.log('game over')
-    gameIsOver = true
-    document.removeEventListener('click', jump)
-    document.removeEventListener('space', jump)
+    clearInterval(timerGame);
+    console.log('game over');
+    gameIsOver = true;
+    document.removeEventListener('click', jump);
+    document.removeEventListener('space', jump);
 }
